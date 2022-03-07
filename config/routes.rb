fec90a0697ccf89de5e81767a2a5c5758d7d93e4
Syeda Juveria Afreen <sja.affu765@gmail.com>
2022-03-07 16:26:00 +0300

    Add files for food list and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
+  root 'foods#index'
   get 'users/index'
   get 'users/sign_out'
   resources :users, only: %i[index]
+  resources :foods, only: [:index, :new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
