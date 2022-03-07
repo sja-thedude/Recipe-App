e4d2b0b5422df87ae0bdcada7a57aafa1a968680
Syeda Juveria Afreen <sja.affu765@gmail.com>
2022-03-07 12:46:22 +0300

    Added Files for Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'users#index'
+  get 'users/index'
+  resources :users, only: %i[index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
