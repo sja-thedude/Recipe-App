1645699f802427cc8a3d33a51b796afe4a7f0a1a
Syeda Juveria Afreen <sja.affu765@gmail.com>
2022-03-07 15:56:53 +0300

    Updated error in the code

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
   get 'users/index'
+  get 'users/sign_out'
   resources :users, only: %i[index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
