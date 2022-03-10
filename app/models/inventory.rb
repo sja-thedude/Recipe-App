62b6ea297b7e1b107eecf3e8014d924760071926
Syeda Juveria Afreen <sja.affu765@gmail.com>
2022-03-10 15:15:20 +0300

    Files updated for the app

---
+++ app/models/inventory.rb
@@ -0,0 +1,7 @@
+class Inventory < ApplicationRecord
+    belongs_to :user
+    has_many :inventory_foods
+  
+    validates :name, :description, presence: true
+  end
+  
--- spec/requests/inventory_foods_spec.rb
+++ spec/requests/inventory_foods_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe 'RecipeFood', type: :request do
+RSpec.describe 'InventoryFoods', type: :request do
+  # no tests included. Tests included in features
   include Devise::Test::IntegrationHelpers
   let(:user) { User.create(name: 'Kolly', email: 'kolly@mail.com', password: 'password') }
-  let(:food) { user.foods.create(name: 'apple', measurementUnit: 'kg', price: 4) }
-  let(:recipe) do
-    user.recipes.create(name: 'Pizza', cookingTime: 1, preparationTime: 2, description: 'hgghghghghh', public: false)
-  end
+  let(:food) { Food.create(name: 'apple', measurementUnit: 'kg', price: 4) }
+  let(:inventory) { Inventory.create(name: 'Pizza', description: 'hgghghghghh', user_id: user.id) }
   describe 'GET /index' do
     before do
       sign_in user
-      get new_recipe_recipe_food_path(recipe)
+      get inventory_inventory_foods_path(inventory)
     end
     it 'should return response status correct (ok)' do
       expect(response).to have_http_status(:ok)
@@ -19,7 +18,7 @@ RSpec.describe 'RecipeFood', type: :request do
       expect(response.content_type).to include 'text/html'
     end
     it 'should include correct placeholder' do
-      expect(response.body).to include('Add Recipe Food')
+      expect(response.body).to include('Add Food')
     end
   end
 end
